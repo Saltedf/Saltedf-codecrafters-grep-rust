145641afec669d1e21beb5fccf5d3d192e65f7c2
salted <saltedf@qq.com>
2025-09-12 23:09:40 +0800

    test: match digits

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,21 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
+    let chars = pattern.chars();
+    // for ch in chars {
+    //     eprint!("{ch}\n");
+    // }
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
     } else {
+        if pattern == r"\d" {
+            for ch in input_line.chars() {
+                if ch.is_digit(10) {
+                    return true;
+                };
+            }
+            return false;
+        }
         panic!("Unhandled pattern: {}", pattern)
     }
 }
