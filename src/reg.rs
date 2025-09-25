8a7dadd66b4ea02e4ac51ca7a09d7d0b0a00e307
salted <saltedf@qq.com>
2025-09-25 22:38:21 +0800

    submit & test

---
+++ src/reg.rs
@@ -0,0 +1,215 @@
+use std::{char, collections::HashSet};
+use thiserror::{self, Error};
+
+#[derive(Debug, Error)]
+pub enum RegError {
+    #[error("'^'只能出现在字符串开头")]
+    MisplacedAnchor,
+
+    #[error("非法的转义字符")]
+    UnknownEscape,
+
+    #[error("未闭合的分组")]
+    UnclosedCharClass,
+}
+
+#[derive(Debug, Clone)]
+enum Inst {
+    Char(char), // one char
+    AnyChar,
+    Start,
+    End,
+    Match,
+    Jump(usize),
+    Split(usize, usize),
+    Group { negated: bool, chars: HashSet<char> },
+    Digit,
+    AlphaNumber,
+}
+
+pub struct Reg {
+    instrs: Vec<Inst>,
+}
+
+impl Reg {
+    pub fn new(pattern: &str) -> Result<Self, RegError> {
+        let instrs = Self::compile(pattern)?;
+        Ok(Self { instrs })
+    }
+
+    pub fn is_match(&self, text: &str) -> bool {
+        match self.instrs.get(0) {
+            Some(Inst::Start) => return self.run(1, text),
+            Some(_) => {
+                let mut text_cursor = text;
+                while !text_cursor.is_empty() {
+                    if self.run(0, text_cursor) {
+                        return true;
+                    }
+                    let mut chars = text_cursor.chars();
+                    chars.next();
+                    text_cursor = chars.as_str();
+                }
+                false
+            }
+            None => return true,
+        }
+    }
+
+    fn run(&self, pc: usize, text: &str) -> bool {
+        match &self.instrs[pc] {
+            Inst::Char(ch) => text.starts_with(*ch) && self.run(pc + 1, &text[ch.len_utf8()..]),
+            Inst::AnyChar => todo!(),
+            Inst::Start => todo!(),
+            Inst::End => text.is_empty() && self.run(pc + 1, text),
+            Inst::Match => true,
+            Inst::Jump(_) => todo!(),
+            Inst::Split(_, _) => todo!(),
+            Inst::Group { negated, chars } => text.chars().nth(0).is_some_and(|c| {
+                let res = if !negated {
+                    chars.contains(&c)
+                } else {
+                    !chars.contains(&c)
+                };
+                res && self.run(pc + 1, &text[c.len_utf8()..])
+            }),
+            Inst::Digit => todo!(),
+            Inst::AlphaNumber => todo!(),
+        }
+    }
+
+    pub fn instrs(&self) -> &Vec<Inst> {
+        &self.instrs
+    }
+
+    fn compile(pattern: &str) -> Result<Vec<Inst>, RegError> {
+        let mut stream = pattern.chars().peekable();
+        let mut instrs = Vec::new();
+        let mut patch_list = Vec::new();
+        // 查看开头是否为 ^
+        if let Some('^') = stream.peek() {
+            instrs.push(Inst::Start);
+            stream.next();
+        }
+
+        while let Some(ch) = stream.next() {
+            match ch {
+                '^' => return Err(RegError::MisplacedAnchor),
+                '\\' => match stream.next() {
+                    Some('d') => instrs.push(Inst::Digit),
+                    Some('w') => instrs.push(Inst::AlphaNumber),
+                    _ => return Err(RegError::UnknownEscape),
+                },
+                '.' | 'a'..='z' => {
+                    let char_inst = match ch {
+                        '.' => Inst::AnyChar,
+                        _ => Inst::Char(ch),
+                    };
+                    if let Some('+') | Some('*') = stream.peek() {
+                        let quantifier = stream.next().expect("非空");
+                        if quantifier == '+' {
+                            instrs.push(char_inst.clone())
+                        }
+                        patch_list.push(instrs.len());
+                        let start = instrs.len();
+                        instrs.push(Inst::Split(start + 1, 0));
+                        instrs.push(char_inst);
+                        instrs.push(Inst::Jump(start));
+                        let pc_to_target = instrs.len();
+                        if let Some(pc_to_patch) = patch_list.pop() {
+                            if let Some(Inst::Split(_, target)) = instrs.get_mut(pc_to_patch) {
+                                *target = pc_to_target;
+                            }
+                        }
+                    } else {
+                        instrs.push(char_inst);
+                    }
+                }
+                '[' => {
+                    let mut chars = HashSet::new();
+                    let mut negated = false;
+                    if let Some('^') = stream.peek() {
+                        negated = true;
+                        stream.next();
+                    }
+                    loop {
+                        match stream.next() {
+                            Some(']') => {
+                                instrs.push(Inst::Group { negated, chars });
+                                break;
+                            }
+                            Some(c) => {
+                                chars.insert(c);
+                            }
+                            None => {
+                                return Err(RegError::UnclosedCharClass);
+                            }
+                        }
+                    }
+                }
+                _ => {
+                    unimplemented!();
+                }
+            }
+        }
+
+        instrs.push(Inst::Match);
+        Ok(instrs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::{Context, Error};
+
+    #[test]
+    fn test_zero_or_more() -> Result<(), Error> {
+        let reg = Reg::new("a*bbbb").context("编译模式串出错")?;
+        let list = reg.instrs();
+        eprintln!("{:?}", list);
+
+        Ok(())
+    }
+    #[test]
+    fn test_compile_group() -> Result<(), Error> {
+        let reg = Reg::new(r"zz[^abc]d\d").context("编译模式串出错")?;
+        let list = reg.instrs();
+        eprintln!("{:?}", list);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_run_chars() -> Result<(), Error> {
+        let reg = Reg::new("abc").context("编译模式串出错")?;
+        let list = reg.instrs();
+        eprintln!("{:?}", list);
+        let res = reg.is_match("abe");
+        assert_eq!(res, true);
+        Ok(())
+    }
+    #[test]
+    fn test_negative_group() -> Result<(), Error> {
+        let reg = Reg::new("[^abc]pple").context("编译模式串出错")?;
+        let list = reg.instrs();
+        eprintln!("{:?}", list);
+        let res = reg.is_match("applepple");
+        assert_eq!(res, true);
+        assert_eq!(reg.is_match(r"apple"), false);
+        assert_eq!(reg.is_match(r"appleapplepple"), true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_postive_group() -> Result<(), Error> {
+        let reg = Reg::new("[abc]pple").context("编译模式串出错")?;
+        let list = reg.instrs();
+        eprintln!("{:?}", list);
+        let res = reg.is_match("epplepple");
+        assert_eq!(res, false);
+        assert_eq!(reg.is_match(r"apple"), true);
+        assert_eq!(reg.is_match(r"pppleapplepple"), true);
+        Ok(())
+    }
+}
