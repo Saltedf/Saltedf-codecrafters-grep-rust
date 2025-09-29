961405bb51f867e15476c99ecbe541bb35550e86
salted <saltedf@qq.com>
2025-09-29 23:54:00 +0800

    submit & test

---
+++ src/regex.rs
@@ -0,0 +1,209 @@
+mod parser;
+
+use anyhow::Error;
+use std::{char, collections::HashSet};
+use thiserror::{self, Error};
+
+use crate::regex::parser::Parser;
+
+#[derive(Debug, Error)]
+pub enum RegexError {
+    #[error("'^'只能出现在字符串开头")]
+    MisplacedAnchor,
+
+    #[error("非法的转义字符")]
+    UnknownEscape,
+
+    #[error("未闭合的分组")]
+    UnclosedCharClass,
+}
+
+#[derive(Debug, Clone)]
+enum Inst {
+    Char(char), // one char
+    AnyChar,
+    Start,
+    End,
+    Match,
+    Jump(usize),
+    Split(usize, usize),
+    Group { negated: bool, chars: HashSet<char> },
+    Digit,
+    MetaChar, // \w : alpha digit '_'
+}
+
+impl Inst {
+    pub fn is_match(&self, ch: &char) -> bool {
+        match self {
+            Inst::Char(c) => *c == *ch,
+            Inst::AnyChar => true,
+            Inst::Start => false,
+            Inst::End => false,
+            Inst::Match => true,
+            Inst::Jump(_) => false,
+            Inst::Split(_, _) => false,
+            Inst::Group { negated, chars } => {
+                if *negated {
+                    !chars.contains(ch)
+                } else {
+                    chars.contains(ch)
+                }
+            }
+            Inst::Digit => ch.is_digit(10),
+            Inst::MetaChar => ch.is_alphanumeric() || *ch == '_',
+        }
+    }
+}
+
+pub struct Regex {
+    instrs: Vec<Inst>,
+}
+
+impl Regex {
+    pub fn new(pattern: &str) -> Result<Self, Error> {
+        let instrs = Parser::new(pattern).compile()?;
+        Ok(Self { instrs })
+    }
+
+    pub fn is_match(&self, text: &str) -> bool {
+        match self.instrs.get(0) {
+            Some(Inst::Start) => self.run(1, text),
+            Some(_) => {
+                let mut text_cursor = text;
+                while !text_cursor.is_empty() {
+                    if self.run(0, text_cursor) {
+                        return true;
+                    }
+                    let mut chars = text_cursor.chars();
+                    chars.next();
+                    text_cursor = chars.as_str();
+                }
+                false
+            }
+            None => true,
+        }
+    }
+
+    fn run(&self, pc: usize, text: &str) -> bool {
+        match &self.instrs[pc] {
+            Inst::Char(ch) => text.starts_with(*ch) && self.run(pc + 1, &text[ch.len_utf8()..]),
+            Inst::AnyChar => todo!(),
+            Inst::Start => todo!(),
+            Inst::End => text.is_empty() && self.run(pc + 1, text),
+            Inst::Match => true,
+            Inst::Jump(target_pc) => self.run(*target_pc, text),
+            Inst::Split(b1, b2) => self.run(*b1, text) || self.run(*b2, text),
+            Inst::Group { negated, chars } => text.chars().nth(0).is_some_and(|c| {
+                let res = if !negated {
+                    chars.contains(&c)
+                } else {
+                    !chars.contains(&c)
+                };
+                res && self.run(pc + 1, &text[c.len_utf8()..])
+            }),
+            Inst::Digit => text
+                .chars()
+                .nth(0)
+                .is_some_and(|c| c.is_digit(10) && self.run(pc + 1, &text[c.len_utf8()..])),
+            Inst::MetaChar => text.chars().nth(0).is_some_and(|c| {
+                (c.is_alphanumeric() || c == '_') && self.run(pc + 1, &text[c.len_utf8()..])
+            }),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::{Context, Error};
+
+    #[test]
+    fn test_compile_zero_or_more() -> Result<(), Error> {
+        let reg = Regex::new("a*bbbb").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+
+        Ok(())
+    }
+    #[test]
+    fn test_compile_group() -> Result<(), Error> {
+        let reg = Regex::new(r"zz[^abc]d\d").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_run_chars() -> Result<(), Error> {
+        let reg = Regex::new("abc").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+        let res = reg.is_match("abc");
+        assert_eq!(res, true);
+        Ok(())
+    }
+    #[test]
+    fn test_negative_group() -> Result<(), Error> {
+        let reg = Regex::new("[^abc]pple").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+        let res = reg.is_match("applepple");
+        assert_eq!(res, true);
+        assert_eq!(reg.is_match(r"apple"), false);
+        assert_eq!(reg.is_match(r"appleapplepple"), true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_postive_group() -> Result<(), Error> {
+        let reg = Regex::new("[abc]pple").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+        let res = reg.is_match("epplepple");
+        assert_eq!(res, false);
+        assert_eq!(reg.is_match(r"apple"), true);
+        assert_eq!(reg.is_match(r"pppleapplepple"), true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_match_escaped_char() -> Result<(), Error> {
+        let reg = Regex::new(r"\d apple").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+        let res = reg.is_match("sally has 3 apples");
+        assert_eq!(res, true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_escaped_char_alphanumber() -> Result<(), Error> {
+        let reg = Regex::new(r"\d \w\w\ws").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+        let res = reg.is_match("sally has 3 dogs");
+        assert_eq!(res, true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_escaped_char_underline() -> Result<(), Error> {
+        let reg = Regex::new(r"\w").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+        eprintln!("{}", '_'.is_alphanumeric());
+        let res = reg.is_match("×#÷_%÷×");
+        assert_eq!(res, true);
+        Ok(())
+    }
+
+    #[test]
+    fn test_match_zero_or_more() -> Result<(), Error> {
+        let reg = Regex::new("a*ab").context("编译模式串出错")?;
+        let list = &reg.instrs;
+        eprintln!("{:?}", list);
+        assert_eq!(reg.is_match("aaacb"), false);
+        Ok(())
+    }
+}
---
+++ src/regex/parser.rs
@@ -0,0 +1,193 @@
+use std::{collections::HashSet, iter::Peekable, str::Chars};
+
+use thiserror::{self, Error};
+
+use crate::regex::Inst;
+
+#[derive(Debug, Error)]
+pub enum ParseError {
+    #[error("未知的转义字符: '\\{0}'")]
+    UnknownEscape(char),
+
+    #[error("不完整的转义字符")]
+    IncompletedEscape,
+
+    #[error("未闭合的字符类，缺少']'")]
+    UnclosedCharClass,
+}
+
+pub struct Parser<'p> {
+    chars: Peekable<Chars<'p>>,
+    instrs: Vec<Inst>,
+    patch_list: Vec<usize>,
+}
+
+impl<'p> Parser<'p> {
+    pub fn new(pattern: &'p str) -> Self {
+        Parser {
+            chars: pattern.chars().peekable(),
+            instrs: Vec::new(),
+            patch_list: Vec::new(),
+        }
+    }
+
+    pub fn compile(mut self) -> Result<Vec<Inst>, ParseError> {
+        self.parse_expr()?;
+        self.instrs.push(Inst::Match);
+        Ok(self.instrs)
+    }
+
+    fn parse_expr(&mut self) -> Result<(), ParseError> {
+        while let Some(_) = self.chars.peek() {
+            self.parse_term()?;
+        }
+        Ok(())
+    }
+
+    fn parse_term(&mut self) -> Result<(), ParseError> {
+        let atom = self.parse_atom()?;
+        match self.chars.peek() {
+            Some('*') => {
+                self.chars.next();
+                self.generate_zero_or_more_code(atom)?;
+            }
+            Some('+') => {
+                self.chars.next();
+                self.generate_one_or_more_code(atom)?;
+            }
+            Some('?') => {
+                self.chars.next();
+                todo!()
+            }
+            Some('{') => todo!(),
+            Some(_) | None => self.instrs.extend(atom.into_iter()),
+        }
+        Ok(())
+    }
+
+    fn parse_atom(&mut self) -> Result<Vec<Inst>, ParseError> {
+        let mut atom_instrs = vec![];
+
+        match self.chars.next() {
+            Some('.') => atom_instrs.push(Inst::AnyChar),
+            Some(ch @ ('a'..='z' | 'A'..='Z' | '0'..='9' | ' ' | '-' | '_')) => {
+                atom_instrs.push(Inst::Char(ch));
+            }
+            Some('\\') => match self.chars.next() {
+                Some('d') => atom_instrs.push(Inst::Digit),
+                Some('w') => atom_instrs.push(Inst::MetaChar),
+                Some(e) => return Err(ParseError::UnknownEscape(e)),
+                None => return Err(ParseError::IncompletedEscape),
+            },
+            Some('[') => {
+                let mut set = HashSet::new();
+                let mut negated = false;
+                if let Some('^') = self.chars.peek() {
+                    negated = true;
+                    self.chars.next();
+                }
+                loop {
+                    match self.chars.next() {
+                        Some(']') => {
+                            atom_instrs.push(Inst::Group {
+                                negated,
+                                chars: set,
+                            });
+                            break;
+                        }
+                        Some('\\') => match self.chars.next() {
+                            Some('d') => set.extend(('0'..='9').into_iter()),
+                            Some('w') => {
+                                set.insert('_');
+                                set.extend(('0'..='9').into_iter());
+                                set.extend(('a'..='z').into_iter());
+                                set.extend(('A'..='Z').into_iter());
+                            }
+                            Some(c) => return Err(ParseError::UnknownEscape(c)),
+                            None => return Err(ParseError::IncompletedEscape),
+                        },
+                        Some(start) if start.is_ascii_alphanumeric() => {
+                            if self.chars.next_if_eq(&'-').is_none() {
+                                set.insert(start);
+                            } else if let Some(end) =
+                                self.chars.next_if(|c| c.is_ascii_alphanumeric())
+                            {
+                                set.extend(Self::translate_range(start, end));
+                            } else {
+                                set.insert('-');
+                            }
+                        }
+                        Some(ch) => {
+                            set.insert(ch);
+                        }
+                        None => return Err(ParseError::UnclosedCharClass),
+                    }
+                }
+            }
+            Some(_) => todo!(),
+            None => todo!(),
+        }
+
+        Ok(atom_instrs)
+    }
+
+    fn pc(&self) -> usize {
+        self.instrs.len()
+    }
+    fn generate_zero_or_more_code(&mut self, atom: Vec<Inst>) -> Result<(), ParseError> {
+        let start = self.pc();
+        let split_code = Inst::Split(start + 1, 0);
+        self.instrs.push(split_code);
+        self.patch_list.push(start);
+        self.instrs.extend(atom.into_iter());
+        let jump_code = Inst::Jump(start);
+        self.instrs.push(jump_code);
+        let pc_to_target = self.pc();
+        if let Some(patch) = self.patch_list.pop() {
+            if let Some(Inst::Split(_, target)) = self.instrs.get_mut(patch) {
+                *target = pc_to_target;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn generate_one_or_more_code(&mut self, atom: Vec<Inst>) -> Result<(), ParseError> {
+        self.instrs.extend(atom.clone().into_iter());
+        self.generate_zero_or_more_code(atom)?;
+        Ok(())
+    }
+    fn generate_zero_or_one_code(&mut self, atom: Vec<Inst>) -> Result<(), ParseError> {
+        todo!();
+        Ok(())
+    }
+
+    fn translate_range(start: char, end: char) -> impl IntoIterator<Item = char> {
+        let is_range = (start as usize <= end as usize)
+            && (start.is_ascii_digit() && end.is_ascii_digit()
+                || start.is_ascii_lowercase() && end.is_ascii_lowercase()
+                || start.is_ascii_uppercase() && end.is_ascii_uppercase());
+
+        if is_range {
+            return (start..=end).collect::<Vec<char>>().into_iter();
+        } else {
+            return vec![start, '-', end].into_iter();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_splice() {
+        let a = '0';
+        let z = 'z';
+        let aa = a as usize;
+        let zz = z as usize;
+        eprintln!("{aa} --> {zz}");
+        let v: Vec<char> = (a..=z).into_iter().collect();
+        eprintln!("{:?}", v);
+    }
+}
